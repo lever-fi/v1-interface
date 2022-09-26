91dbeea376dcb36bc7a3136d8ceb9f2b35f35bd7
prathik <37804760+prmali@users.noreply.github.com>
2022-09-25 17:59:35 -0700

    Update NavElement.js

--- src/components/Topbar/NavElement.js
+++ src/components/Topbar/NavElement.js
@@ -1,7 +1,7 @@
 const NavElement = ({ name, to }) => {
 	return (
 		<div className="relative">
-			<p className="text-white font-medium cursor-pointer text-transparent bg-clip-text bg-gradient-to-bl from-neutral-100 to-neutral-300">
+			<p className="text-primary-content font-medium cursor-pointer text-transparent bg-clip-text bg-gradient-to-bl from-neutral-100 to-neutral-300">
 				{name}
 			</p>
 			<div className="absolute t-full w-full h-px bg-white"></div>
